262354298790e44d6f9cba423d0348a24682597c
Tim Randall <trtimrandall@gmail.com>
2021-10-14 01:28:49 +1100

    some ui stuff

--- MathsGardenios/ContentView.swift
+++ MathsGardenios/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Background()
+            VStack{
+                Title(words: "Title test")
+                Text("Hello, world!")
+                    .padding()
+                ButtonWidget(words: "test")
+        }
+        }
     }
 }
 
---
+++ MathsGardenios/Stuff.swift
@@ -0,0 +1,34 @@
+//
+//  Stuff.swift
+//  MathsGardenios
+//
+//  Created by Tim Randall on 14/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct ButtonWidget: View {
+    var words: String
+    var body: some View {
+        Text(words)
+            .font(.system(size: 18, weight: .bold))
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.green)
+            .cornerRadius(30)
+    }
+}
+
+struct Background: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.white, .white, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+
+struct Title: View {
+    var words: String
+    var body: some View {
+        Text(words).font(.system(size: 24, weight: .bold))
+    }
+}
